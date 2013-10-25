70507f37cbfeefb84adf916a9f826a3172d0e950
Trey Cordova <treycordova@hotmail.com>
2013-10-25 17:05:26 -0600

    Update default level;

--- default.lua
+++ default.lua
@@ -1,61 +1,6 @@
-local level = {}
-
-----------------
--- Meta
-----------------
-level.name = 'default'
-level.px = 96
-level.py = 400
-level.width = 2000
-level.height = 600
-
-----------------
--- Mask
-----------------
-level.walls = {
-	{
-		0, 200,
-		0, 1600,
-		700, 1600,
-		700, 1300,
-		900, 1300,
-		900, 1600,
-		1200, 1600,
-		1200, 1300,
-		1500, 1300,
-		1500, 900,
-		1800, 900,
-		1800, 600,
-		900, 600,
-		900, 900,
-		700, 900,
-		700, 700,
-		900, 700,
-		900, 200
-	},
-	{
-		1100, 800,
-		1100, 1000,
-		1300, 1000,
-		1300, 800
-	},
-	{
-		200, 900,
-		200, 1100,
-		500, 1100,
-		500, 900
+return {
+	entities = {
+		{entity = 'Wall', data = {x = 400, y = 400, w = 400, h = 100}},
+		{entity = 'Player', data = {x = 400, y = 100, w = 10, h = 20}}
 	}
 }
-
-level.seeds = {
-	{200, 400, Mushroom},
-	{300, 400, Bridge},
-	{400, 400, Dropper}
-}
-
---Even though it says entities, you can pretty much use this to make whatever you want
-level.entities = {
-
-}
-
-return level
