9c5bd51c6e9a19fe448ad70790cdb69296601bc1
tie372 <tie.372@gmail.com>
2013-10-26 17:03:52 -0600

    Made player fat.

--- default.lua
+++ default.lua
@@ -1,6 +1,6 @@
 return {
 	entities = {
 		{entity = 'Wall', data = {x = 400, y = 400, points = {-200, -100, -200, 100, 200, 100, 200, -100}}},
-		{entity = 'Player', data = {x = 400, y = 100, w = 20, h = 40}}
+		{entity = 'Player', data = {x = 400, y = 100, w = 50, h = 40}}
 	}
 }
